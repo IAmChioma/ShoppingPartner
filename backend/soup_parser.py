0cfe0414c625d42966d8e68a4b85c980879279ce
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2024-10-19 14:18:57 -0500

    Return json data

--- backend/soup_parser.py
+++ backend/soup_parser.py
@@ -1,23 +1,41 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 
 def walmart_json(soup: BeautifulSoup):
+    result = []
     items = soup.find_all(attrs={"data-testid": "item-stack"})
     if (items.__len__() == 0):
         print("Didn't find item stack")
-        return None
+        return json.dumps(result)
     for item in items[0].children:
         intermediate = first_child(first_child(item))
         if (intermediate is None):
             break
         link = walmart_link_cleaner(first_child(intermediate)["href"])
         if (len(link) == 0):
-            break;
+            break
         #print(link)
-    
-    # TODO: return data as JSON
-    pass
+        img_url = intermediate.find("img")["src"]
+        #print(img_url)
+        priceTag = intermediate.find(attrs={"data-automation-id": "product-price"})
+        price = "n/a"
+        i = 0
+        for child in priceTag.children:
+            if (i == 1):
+                price = child.contents
+                break
+            i += 1
+        price = price[0]
+        price = price[price.find("$"):len(price)]
+        #print(price)
+        titleTag = intermediate.find(attrs={"data-automation-id": "product-title"})
+        title = titleTag.contents[0]
+        #print(title)
+        data = {"link": link, "img_url": img_url, "price": price, "title": title}
+        result.append(data)
+    return json.dumps(result)
 
 
 def walmart_link_cleaner(url):
